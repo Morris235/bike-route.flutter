0595c9e8ad07520c15b2ad1e005ea05a76834c63
morris <dltkdah777@gmail.com>
2024-08-01 14:23:03 +0900

    refactor : 디버그 모드일 때만 로그 표시

--- lib/logging.dart
+++ lib/logging.dart
@@ -1,4 +1,5 @@
 // 전역 로거 인스턴스
+import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
 final Logger logger = Logger('GraphQLLogger');
@@ -7,12 +8,18 @@ final Logger logger = Logger('GraphQLLogger');
 void setupLogging() {
   Logger.root.level = Level.ALL; // 전체 로그 레벨 설정
   Logger.root.onRecord.listen((LogRecord rec) {
-    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+    if (kDebugMode) {
+      print('${rec.level.name}: ${rec.time}: ${rec.message}');
+    }
     if (rec.error != null) {
-      print('Error: ${rec.error}');
+      if (kDebugMode) {
+        print('Error: ${rec.error}');
+      }
     }
     if (rec.stackTrace != null) {
-      print('StackTrace: ${rec.stackTrace}');
+      if (kDebugMode) {
+        print('StackTrace: ${rec.stackTrace}');
+      }
     }
   });
 }
